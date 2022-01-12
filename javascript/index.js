b500ed537b37b0f01696d561f95788129fed0991
Suraj Mishra <mishrasur7@gmail.com>
2022-01-12 15:40:51 +0200

    function added

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,30 @@
 function clickMenu() {
     var menu = document.getElementById("menu"); 
+    var home = document.getElementById("home");
+    var about = document.getElementById("about"); 
+
 
     if (menu.style.display == "block") {
         menu.style.display = "none"; 
+        
     } else {
         menu.style.display = "block"; 
-    }
+        home.style.marginTop = "250px";     
+    } 
 }
+
+function clickMenuAbout() {
+    var menu = document.getElementById("menu"); 
+    var about = document.getElementById("about"); 
+
+
+    if (menu.style.display == "block") {
+        menu.style.display = "none"; 
+        
+    } else {
+        menu.style.display = "block"; 
+        about.style.marginTop = "300px"; 
+    } 
+}
+
+
